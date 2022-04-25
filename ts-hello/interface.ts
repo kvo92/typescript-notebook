4c2aba262fac59fa166a3ab5e2d28f2730c7e6a1
kvo92 <88671104+kvo92@users.noreply.github.com>
2022-04-25 16:49:15 -0400

    constructor

--- ts-hello/interface.ts
+++ ts-hello/interface.ts
@@ -6,8 +6,18 @@
 
 // let drawPoint = (point: Point) => {};
 class Point {
-  x: number;
-  y: number;
-  draw() {}
+  x?: number;
+  y?: number;
+  constructor(x?: number, y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    console.log("X: " + this.x + ", Y: " + this.y);
+  }
   getDistance(another: Point) {}
 }
+
+let point = new Point(2);
+
+point.draw();
